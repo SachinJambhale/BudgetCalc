0675f4efc4c90db97f2ac468d1faac076bcf4106
sachin <jambhalesachin39@gmail.com>
2023-05-18 23:11:59 +0530

    second commit by sachin

---
+++ my-app/src/components/wp/WbsTabView.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface IWbsTabViewProps {}
+
+const WbsTabView: React.FunctionComponent<IWbsTabViewProps> = (props) => {
+  return <></>;
+};
+
+export default WbsTabView;
