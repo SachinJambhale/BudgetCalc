6409458f608540704c218362cdd89fcc4542fec2
FrontEndLalit <frontendlalit@gmail.com>
2023-06-09 18:26:06 +0530

    changes by lalit

--- my-app/src/components/wp/tables/ProjectBillRateTable.tsx
+++ my-app/src/components/wp/tables/ProjectBillRateTable.tsx
@@ -1,16 +1,9 @@
-
-import React, { useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import Select from "@mui/material/Select";
+import React, { ChangeEvent, useState,useEffect } from "react";
 import TableContainer from "@mui/material/TableContainer";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import GradeOutlinedIcon from "@mui/icons-material/GradeOutlined";
 import GradeIcon from "@mui/icons-material/Grade";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import DownIcon from "../Icons/DownIcon";
 import RightIcon from "../Icons/RightIcon";
@@ -28,15 +21,21 @@ const ProjectBillRateTable = () => {
   const [isCollapsibleOpen, setCollapsibleOpen] = useState(false);
   const [isEditing, setEditing] = useState(false);
   const [showInvalidRates, setShowInvalidRates] = useState(true);
+  const [data, setData] = useState(obj);
+  const _ = require("lodash");
+  const copiedObject = _.cloneDeep(data);
+  const [tempData, setTempData] = React.useState(copiedObject);
+  
 
   const handleEdit = () => {
     setEditing(true);
   };
 
   const handleSave = () => {
-    // Logic to save the changes
+    setData({ ...tempData });
     setEditing(false);
   };
+
 
   const handleCancel = () => {
     // Logic to cancel the changes
@@ -52,12 +51,16 @@ const ProjectBillRateTable = () => {
   const handleToggleRow = (rowIndex: number) => {
     if (rowIndex === -1) {
       // Clicked on the header
-      if (openRows.length === data.value.length) {
+      if (
+        openRows.length === data.calcRev.AP4_BudgetCalcRateSchRel.value.length
+      ) {
         // All rows are open, so close all
         setOpenRows([]);
       } else {
         // Some rows are closed, so open all
-        const allRows = data.value.map((_, index) => index);
+        const allRows = data.calcRev.AP4_BudgetCalcRateSchRel.value.map(
+          (_, index) => index
+        );
         setOpenRows(allRows);
       }
     } else {
@@ -70,11 +73,6 @@ const ProjectBillRateTable = () => {
     }
   };
 
-  const handleDeleteRow = (rowIndex: number) => {
-    const updatedData = [...data.value];
-    updatedData.splice(rowIndex, 1);
-    setData({ ...data, value: updatedData });
-  };
 
   const isRowOpen = (rowIndex: number) => {
     return openRows.includes(rowIndex);
@@ -83,8 +81,24 @@ const ProjectBillRateTable = () => {
     setShowInvalidRates(!showInvalidRates);
   };
 
-  const [data, setData] = useState(obj);
+
   console.log(data);
+ 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    e.preventDefault();
+    const name = e.target.name as string;
+    const { value } = e.target;
+    // tempData.ctrRevision.calcRev.ctrs[index][name].value[0] = value;\
+    tempData.calcRev.AP4_BudgetCalcRateSchRel.value[index][name].value[0] = value;
+    setTempData({ ...tempData });
+    console.log("initialData", data);
+    console.log("Temp", tempData);
+  };
+
+  useEffect(() => {
+    setTempData(_.cloneDeep(data));
+  }, [data]);
+
   const header = [
     "Rate Table Table",
     "Category Type",
@@ -98,11 +112,9 @@ const ProjectBillRateTable = () => {
     "Valid From",
     "Valid To",
   ];
-
   return (
     <>
-
-       <Box sx={{ margin: "5px", display: "flex", justifyContent: "start" }}>
+      <Box sx={{ margin: "5px", display: "flex", justifyContent: "start" }}>
         {!isEditing ? (
           <Button
             className="custombtn"
@@ -137,39 +149,36 @@ const ProjectBillRateTable = () => {
           {showInvalidRates ? "SHOW INVALID RATES" : "HIDE INVALID RATES"}
         </Button>
       </Box>
-      <TableContainer component={Paper}>
-        <Table className="cells" aria-label="simple table">
-          <TableHead className="tableHead">
-            <TableRow>
-              <TableCell style={{ width: 200 }}>
+      <TableContainer>
+        <table className="cells_project_rate" aria-label="simple table">
+            <tr>
+              <th>
                 <IconButton onClick={() => handleToggleRow(-1)}>
-                  {openRows.length === data.value.length ? (
+                  {openRows.length ===
+                  data.calcRev.AP4_BudgetCalcRateSchRel.value.length ? (
                     <DownIcon />
                   ) : (
                     <RightIcon />
                   )}
                 </IconButton>
-              </TableCell>
+              </th>
               {header.map((head, index) => (
-                <TableCell key={head} sx={{ fontSize: "0.9rem",width: 120 }}>
+                <th key={head}  className="tborder tableHead"
+                style={{ fontSize: "0.9rem", fontWeight: "bold" }}>
                   {head}
-                </TableCell>
+                </th>
               ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.value.map((item, index) => (
+            </tr>
+            {tempData.calcRev.AP4_BudgetCalcRateSchRel.value.map((item : any, index: any) => (
               <React.Fragment key={index}>
-                <TableRow sx={{width: 160, border: 1 }}>
-                  <TableCell>
+                <tr>
+                  <td>
                     <IconButton onClick={() => handleToggleRow(index)}>
                       {isRowOpen(index) ? <DownIcon /> : <RightIcon />}
                     </IconButton>
                     <AddCircleOutlineIcon style={{ color: "#2cb9ec" }} />{" "}
                     <DeleteOutlineOutlinedIcon
-                      style={{ color: "#2cb9ec" }}
-                      onClick={() => handleDeleteRow(index)}
-                    />
+                      style={{ color: "#2cb9ec" }}/>
                     {favourite[index] ? (
                       <GradeIcon
                         style={{ color: "#2cb9ec" }}
@@ -181,141 +190,108 @@ const ProjectBillRateTable = () => {
                         onClick={() => handleFav(index)}
                       />
                     )}
-                  </TableCell>
-                  <TableCell>{item.object_name.value}</TableCell>
-                  <TableCell style={{ width: 200, padding: 0 }}>
-                    <FormControl variant="outlined" fullWidth>
-                      <InputLabel htmlFor="outlined-age-native-simple">
-                        {item.ap4_rateschedule_type_cat.value}
-                      </InputLabel>
-                      <Select native label="Value" className="selectDropdown">
-                        <option aria-label="None" value="" />
-                        <option>Internal category</option>
+                  </td>
+                  <td
+                    className={`td  ${isEditing ? "editable" : ""}`}
+                    align="right"
+                  >
+                    {isEditing ? (
+                      <input
+                        className="cells"
+                        name={item.object_name.name}
+                        type="text"
+                        value={item.object_name.value[0]}
+                        onChange={(e) => handleChange(e, index)}
+                      />
+                    ) : (
+                      item.object_name.value[0]
+                    )}
+                  </td>
+                  <td style={{ width: 200, padding: 0 }}>
+                    
+                      <select name="Internal category" id="cat1">
+                        <option>{item.ap4_rateschedule_type_cat.value}</option>
                         <option>Customer category</option>
                         <option>Project role</option>
-                      </Select>
-                    </FormControl>
-                  </TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
-                  <TableCell></TableCell>
+                      </select>
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
                   {/* Add more cells for other data properties */}
-                </TableRow>
+                </tr>
                 {/* Render additional rows if the current row is open */}
                 {isRowOpen(index) && (
                   <React.Fragment>
-                    <TableRow>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell style={{ width: 200, padding: 0 }}>
-                    <FormControl variant="outlined" fullWidth>
-                      <InputLabel htmlFor="outlined-age-native-simple">
-                      {item.jobCategories[0]?.ap4_OracleJobCategory?.value[0]}
-                      </InputLabel>
-                      <Select native label="Value" className="selectDropdown">
-                        <option aria-label="None" value="" />
-                        <option>ASSIST..</option>
-                        <option>PRESV..</option>
-                        <option>PREV..</option>
-                        <option>PRIN..</option>
-                        <option>PROF..</option>
-                        <option>SEN..</option>
-                        <option>SPRIN..</option>
-                        <option>XSUB..</option>
-                        <option>XTEMP.</option>
-                      </Select>
-                    </FormControl>
-                    </TableCell>
-                      <TableCell>
-                        {item.AP4_RateSchedJobCatRelation?.value[0]?.object_name?.value.map(
-                          (data, index) => (
-                            <div key={index}>{data}</div>
-                          )
-                        )}
-                      </TableCell>
-                      <TableCell>{item.ap4_currencycode.value[0]}</TableCell>
+                    {item?.jobCategories?.map((insideCate:any,index:any)=>(<tr hidden={ insideCate?.ap4_to_date?.value[0] === null || !showInvalidRates? false:true}>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td style={{ width: 200, padding: 0 }}>
+                      <select name="Internal category" id="cat1">
+                        <option> { item.jobCategories[0]?.ap4_OracleJobCategory?.value}</option>
+                            <option>ASSIST..</option>
+                            <option>PRESV..</option>
+                            <option>PREV..</option>
+                            <option>PRIN..</option>
+                            <option>PROF..</option>
+                            <option>SEN..</option>
+                            <option>SPRIN..</option>
+                            <option>XSUB..</option>
+                            <option>XTEMP.</option>
+                      </select>
+                      </td>
+                      <td>{insideCate?.object_name?.value}</td>
+                      <td>{insideCate?.ap4_currencycode?.value}</td>
 
-                      <TableCell>{item.jobCategories[0]?.ap4_external_rate?.value[0]}</TableCell>
-                      <TableCell>{item.jobCategories[0]?.object_desc?.value[0]}</TableCell>
-                      <TableCell>{item.jobCategories[0]?.ap4_years_of_exp_from?.value[0]}</TableCell>
-                      <TableCell>{item.jobCategories[0]?.ap4_years_of_exp_to?.value[0]}</TableCell>
-                      <TableCell>{isEditing?(<input
-                        className="cells"
-                        name= "From Date"
-                        type="date"
-                        value={item.jobCategories[0]?.ap4_from_date?.value[0].split("T")[0]}
-                      />):(item.jobCategories[0]?.ap4_from_date?.value[0].split("T")[0])}
-                      </TableCell>
-                      <TableCell>
-                      {item.jobCategories[0]?.ap4_to_date?.value[0]}
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell></TableCell>
-                      <TableCell style={{ width: 200, padding: 0 }}>
-                    <FormControl variant="outlined" fullWidth>
-                      <InputLabel htmlFor="outlined-age-native-simple">
-                      {item.jobCategories[0]?.ap4_OracleJobCategory?.value[0]}
-                      </InputLabel>
-                      <Select native label="Value" className="selectDropdown">
-                        <option aria-label="None" value="" />
-                        <option>ASSIST..</option>
-                        <option>PRESV..</option>
-                        <option>PREV..</option>
-                        <option>PRIN..</option>
-                        <option>PROF..</option>
-                        <option>SEN..</option>
-                        <option>SPRIN..</option>
-                        <option>XSUB..</option>
-                        <option>XTEMP.</option>
-                      </Select>
-                    </FormControl>
-                    </TableCell>
-                      <TableCell>
-                        {item.AP4_RateSchedJobCatRelation?.value[0]?.object_name?.value.map(
-                          (data, index) => (
-                            <div key={index}>{data}</div>
-                          )
+                      <td>
+                        {insideCate?.ap4_external_rate?.value}
+                      </td>
+                      <td>
+                        {insideCate?.object_desc?.value}
+                      </td>
+                      <td>
+                        {insideCate?.ap4_years_of_exp_from?.value}
+                      </td>
+                      <td>
+                        {insideCate?.ap4_years_of_exp_to?.value}
+                      </td>
+                      <td>
+                        {isEditing ? (
+                          <input
+                            className="cells"
+                            name={insideCate?.ap4_from_date?.name}
+                            type="date"
+                            value={
+                            insideCate.ap4_from_date?.value[0]?.split(
+                                "T"
+                              )[0]
+                            }
+                          />
+                        ) : (
+                          insideCate?.ap4_from_date?.value[0]?.split(
+                            "T"
+                          )[0]
                         )}
-                      </TableCell>
-                      <TableCell>{item.ap4_currencycode.value[0]}</TableCell>
-
-                      <TableCell>{item.jobCategories[0]?.ap4_external_rate?.value[0]}</TableCell>
-                      <TableCell>{item.jobCategories[0]?.object_desc?.value[0]}</TableCell>
-                      <TableCell>{item.jobCategories[0]?.ap4_years_of_exp_from?.value[0]}</TableCell>
-                      <TableCell>{item.jobCategories[0]?.ap4_years_of_exp_to?.value[0]}</TableCell>
-                      <TableCell>{isEditing?(<input
-                        className="cells"
-                        name= "From Date"
-                        type="date"
-                        value={item.jobCategories[0]?.ap4_from_date?.value[0].split("T")[0]}
-                      />):(item.jobCategories[0]?.ap4_from_date?.value[0].split("T")[0])}
-                      </TableCell>
-                      <TableCell>
-                        {item.jobCategories[0]?.ap4_to_date?.value[0]}
-                      </TableCell>
-                    </TableRow>
+                      </td>
+                      <td>
+                        {insideCate?.ap4_to_date?.value[0]?.split("T")[0]}
+                      </td>
+                      </tr>))}
+                 
                   </React.Fragment>
                 )}
               </React.Fragment>
             ))}
-          </TableBody>
-        </Table>
+        </table>
       </TableContainer>
-
     </>
   );
 };
 
 export default ProjectBillRateTable;
-
-
-
