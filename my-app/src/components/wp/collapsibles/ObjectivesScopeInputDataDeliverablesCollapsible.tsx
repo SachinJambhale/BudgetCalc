ce1365561b436aff1f38ca7737c1cca2b7f1547f
poojajanke <poojajanke95@gmail.com>
2023-06-20 16:18:09 +0530

    commitedbypooja

--- my-app/src/components/wp/collapsibles/ObjectivesScopeInputDataDeliverablesCollapsible.tsx
+++ my-app/src/components/wp/collapsibles/ObjectivesScopeInputDataDeliverablesCollapsible.tsx
@@ -1,4 +1,78 @@
 
+// import React, { useState } from "react";
+// import "./WorkPackageCollapsible.css";
+// import IconButton from "@mui/material/IconButton";
+// import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+// import DownIcon from "../Icons/DownIcon";
+// import RightIcon from "../Icons/RightIcon";
+ //import ObjectivesScopeInputDataDeliverablesTable from "../tables/ObjectivesScopeInputDataDeliverablesTable";
+
+// interface IProjectBillRateProps {}
+
+// const ObjectivesScopeInputDataDeliverablesCollapsible: React.FunctionComponent<IProjectBillRateProps> = () => {
+//   const [isCollapsibleOpen, setCollapsibleOpen] = useState(false);
+//   const [isEditing, setEditing] = useState(false);
+
+//   const handleEdit = () => {
+//     setEditing(true);
+//   };
+
+//   const handleSave = () => {
+//     // Logic to save the changes
+//     setEditing(false);
+//   };
+
+//   const handleCancel = () => {
+//     // Logic to cancel the changes
+//     setEditing(false);
+//   };
+
+//   const handleCollapsibleClick = () => {
+//     setCollapsibleOpen(!isCollapsibleOpen);
+//   };
+
+//   // ...
+
+//   return (
+//     <>
+//       <div>
+//         <div className="collapsible" onClick={handleCollapsibleClick}>
+//           <h4 className="icon">{isCollapsibleOpen ? <DownIcon /> : <RightIcon />} </h4>
+//           <h4 className="collapseHead">OBJECTIVES-SCOPE-INPUT DATA-DELIVERABLES</h4>
+          
+//         </div>
+
+//         {isCollapsibleOpen && (
+//           <Box>
+//             <Box
+//               sx={{ margin: "5px", display: "flex", justifyContent: "start" }}
+//             >
+//               {!isEditing ? (
+//                 <Button className="custombtn" sx={{ margin: "10px" }} onClick={handleEdit}>
+//                   EDIT
+//                 </Button>
+//               ) : (
+//                 <>
+//                   <Button className="custombtn" sx={{ margin: "10px" }} onClick={handleSave}>
+//                     SAVE
+//                   </Button>
+//                   <Button className="custombtn" sx={{ margin: "10px" }} onClick={handleCancel}>
+//                     CANCEL
+//                   </Button>
+//                 </>
+//               )}
+//             </Box>
+//             <ObjectivesScopeInputDataDeliverablesTable />
+//           </Box>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ObjectivesScopeInputDataDeliverablesCollapsible;
+
 import React, { useState } from "react";
 import "./WorkPackageCollapsible.css";
 import IconButton from "@mui/material/IconButton";
@@ -6,28 +80,11 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import DownIcon from "../Icons/DownIcon";
 import RightIcon from "../Icons/RightIcon";
-import ObjectivesScopeInputDataDeliverablesTable from "../tables/ObjectivesScopeInputDataDeliverablesTable";
 
-interface IProjectBillRateProps {}
+interface IObjectivesScopeInputDataDeliverablesCollapsibleProps {}
 
-const ObjectivesScopeInputDataDeliverablesCollapsible: React.FunctionComponent<IProjectBillRateProps> = () => {
+const ObjectivesScopeInputDataDeliverablesCollapsible: React.FunctionComponent<IObjectivesScopeInputDataDeliverablesCollapsibleProps> = () => {
   const [isCollapsibleOpen, setCollapsibleOpen] = useState(false);
-  const [isEditing, setEditing] = useState(false);
-
-  const handleEdit = () => {
-    setEditing(true);
-  };
-
-  const handleSave = () => {
-    // Logic to save the changes
-    setEditing(false);
-  };
-
-  const handleCancel = () => {
-    // Logic to cancel the changes
-    setEditing(false);
-  };
-
   const handleCollapsibleClick = () => {
     setCollapsibleOpen(!isCollapsibleOpen);
   };
@@ -40,30 +97,11 @@ const ObjectivesScopeInputDataDeliverablesCollapsible: React.FunctionComponent<I
         <div className="collapsible" onClick={handleCollapsibleClick}>
           <h4 className="icon">{isCollapsibleOpen ? <DownIcon /> : <RightIcon />} </h4>
           <h4 className="collapseHead">OBJECTIVES-SCOPE-INPUT DATA-DELIVERABLES</h4>
-          
         </div>
 
         {isCollapsibleOpen && (
           <Box>
-            <Box
-              sx={{ margin: "5px", display: "flex", justifyContent: "start" }}
-            >
-              {!isEditing ? (
-                <Button className="custombtn" sx={{ margin: "10px" }} onClick={handleEdit}>
-                  EDIT
-                </Button>
-              ) : (
-                <>
-                  <Button className="custombtn" sx={{ margin: "10px" }} onClick={handleSave}>
-                    SAVE
-                  </Button>
-                  <Button className="custombtn" sx={{ margin: "10px" }} onClick={handleCancel}>
-                    CANCEL
-                  </Button>
-                </>
-              )}
-            </Box>
-            <ObjectivesScopeInputDataDeliverablesTable />
+              < ObjectivesScopeInputDataDeliverablesTable/>
           </Box>
         )}
       </div>
@@ -72,5 +110,3 @@ const ObjectivesScopeInputDataDeliverablesCollapsible: React.FunctionComponent<I
 };
 
 export default ObjectivesScopeInputDataDeliverablesCollapsible;
-
-
